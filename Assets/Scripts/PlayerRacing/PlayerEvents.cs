38c357da6d3b9a8d47a7887871da57bd9474582d
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-06 13:58:26 -0800

    remove unused lines

--- Assets/Scripts/PlayerRacing/PlayerEvents.cs
+++ Assets/Scripts/PlayerRacing/PlayerEvents.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class PlayerEvents : MonoBehaviour
 {
-    public delegate void PauseMoveAnimation();
-    public PauseMoveAnimation onPauseMoveAnimation;
-
     public delegate void StartDrift(float input);
     public StartDrift onStartDrift;
     public delegate void EndDrift();
     public EndDrift onEndDrift;
-
-    public delegate void SpeedBoost(float boost, Rigidbody rb, Vector3 direction);
 }
