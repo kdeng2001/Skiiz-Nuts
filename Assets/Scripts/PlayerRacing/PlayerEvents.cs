6f8233dcbf5a542c529cae8ecd564730717695c4
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-06 14:06:11 -0800

    summary comment for PlayerEvents

--- Assets/Scripts/PlayerRacing/PlayerEvents.cs
+++ Assets/Scripts/PlayerRacing/PlayerEvents.cs
@@ -2,6 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Declares delegates invoked on certain player racing driven inputs,
+/// like starting or ending drifting.
+/// </summary>
 public class PlayerEvents : MonoBehaviour
 {
     public delegate void StartDrift(float input);
