8a67294b019b0db5f15cce7fa6b1932ee1f8c744
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-08 20:49:51 -0800

    add summary comments for Steer class and methods

--- Assets/Scripts/PlayerRacing/Steer.cs
+++ Assets/Scripts/PlayerRacing/Steer.cs
@@ -3,6 +3,10 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Handles the direction the player moves, as well as sprite rotation based on this direction and the ground.
+/// </summary>
 public class Steer : MonoBehaviour
 {
     private Rigidbody rb;    
@@ -13,6 +17,10 @@ public class Steer : MonoBehaviour
     [Tooltip("The base rate at which the player can steer left or right. This variable should not be modified.")]
     [SerializeField] public float baseSteerRate = 2;
     public float steerRate;
+    
+    /// <summary>
+    /// Finds necessary components and initializes the current angle and steerRate.
+    /// </summary>
     private void Awake()
     {
         rb = transform.parent.GetComponentInChildren<Rigidbody>();
@@ -21,8 +29,9 @@ public class Steer : MonoBehaviour
         direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * (yAngle - 90)), 0, Mathf.Sin(Mathf.Deg2Rad * (yAngle + 90)));
         steerRate = baseSteerRate;
     }
+
     /// <summary>
-    /// Steers player in a given direction to allow turning of corners
+    /// Steers player in a given direction to allow turning of corners.
     /// </summary>
     /// <param name="inputDirection">-1,0,1 depending on horizontal input of player </param>
     public void Steering(float inputDirection)
@@ -34,6 +43,9 @@ public class Steer : MonoBehaviour
         //SphereDebug();
     }
 
+    /// <summary>
+    /// Determines direction of movement and rotation of sprite relative to the ground normal
+    /// </summary>
     public void SetDirection(float inputDirection)
     {
         direction = new Vector3(Mathf.Cos(Mathf.Deg2Rad * (yAngle - 90)), 0,
@@ -43,7 +55,7 @@ public class Steer : MonoBehaviour
         if (Physics.Raycast(transform.position, Vector3.down, out hit, Mathf.Infinity, ground.groundCheckLayerMask) && ground.IsGrounded(transform.up * -1))
         {
             GroundedMovement(hit);
-            // X and Z axis rotation depending on ground
+            // X and Z axis rotation of sprite depending on ground
             float z = GetAngleFromWorld(hit.normal, transform.up, newDirection);
             float x = GetAngleFromWorld(newDirection, transform.forward);
             Quaternion targetRotation = Quaternion.Euler(new (transform.eulerAngles.x + x, yAngle, transform.eulerAngles.z + z));
@@ -59,6 +71,12 @@ public class Steer : MonoBehaviour
         RotatePlayerSpriteY();
     }
 
+    /// <summary>
+    /// Computes the angle between two vectors.
+    /// </summary>
+    /// <param name="targetDirection"> The direction vector the player will face. </param>
+    /// <param name="currentDirection"> The direction vector the player currently faces. </param>
+    /// <returns> Returns the angle between targetDirection and currentDirection. </returns>
     private float GetAngleFromWorld(Vector3 targetDirection, Vector3 currentDirection)
     {
         Vector3 axis = Vector3.Cross(currentDirection, targetDirection);
@@ -67,11 +85,22 @@ public class Steer : MonoBehaviour
         return -1 * angle;
     }
 
+    /// <summary>
+    /// Computes the angle between two vectors, in relation to a third axis vector.
+    /// </summary>
+    /// <param name="targetDirection"> The direction vector the player will face. </param>
+    /// <param name="currentDirection"> The direction vector the player currently faces. </param>
+    /// <param name="axis"></param>
+    /// <returns> Returns the angle between targetDirection and currentDirection about a third axis vector. </returns>
     private float GetAngleFromWorld(Vector3 targetDirection, Vector3 currentDirection, Vector3 axis)
     {
         float angle = Vector3.SignedAngle(currentDirection, targetDirection, axis);
         return  angle;
     }
+    
+    /// <summary>
+    /// Draws lines for debugging important vectors that will be manipulated with vector math.
+    /// </summary>
     private void SphereDebug()
     {
         Debug.DrawLine(transform.position, transform.position + 10 * rb.transform.forward, Color.red);
@@ -79,27 +108,39 @@ public class Steer : MonoBehaviour
         Debug.DrawLine(transform.position, transform.position + 10 * rb.transform.right, Color.blue);
     }
     
+    /// <summary>
+    /// For additional vector lines that can be drawn to help debug and verify correct use of vector math.
+    /// </summary>
     private void OtherDebugs()
     {
         Debug.DrawLine(transform.position, transform.position + 10 * transform.forward, Color.red);
     }
+
+    /// <summary>
+    /// Handles direction to apply velocity when the player is on the ground.
+    /// </summary>
+    /// <param name="ground"></param>
     private void GroundedMovement(RaycastHit ground)
     {
         Vector3 projected = Vector3.Project(direction, ground.normal);
         newDirection = (direction - projected).normalized;
-        //rb.velocity = rb.velocity.magnitude * newDirection;
         rb.AddForce(-rb.velocity + rb.velocity.magnitude * newDirection, ForceMode.VelocityChange);
     }
 
+    /// <summary>
+    /// Handles direction to apply velocity when the player is above ground.
+    /// </summary>
     private void InAirMovement()
     {
-        //newDirection = direction;
         Vector3 yVelocity = Vector3.up * rb.velocity.y;
         Vector3 xzVelocity = new Vector3(rb.velocity.x, 0, rb.velocity.z);
         newDirection = (xzVelocity + yVelocity).normalized;
-        //rb.velocity = directionVelocity.magnitude * direction + yVelocity;
         rb.AddForce(-rb.velocity + xzVelocity.magnitude * direction + yVelocity, ForceMode.VelocityChange);
     }
+
+    /// <summary>
+    /// Rotates the player sprite along the y axis only.
+    /// </summary>
     private void RotatePlayerSpriteY() 
     { 
         transform.eulerAngles = new(transform.eulerAngles.x, yAngle, transform.eulerAngles.z);
