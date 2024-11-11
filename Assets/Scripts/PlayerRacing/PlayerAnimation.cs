8c7f0f479ec683c6cea240d01e889fbef2719222
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-11 13:18:51 -0800

    Update PlayerAnimation class with  summaries of class, methods, properties

--- Assets/Scripts/PlayerRacing/PlayerAnimation.cs
+++ Assets/Scripts/PlayerRacing/PlayerAnimation.cs
@@ -2,11 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// PlayerAnimation contains methods for playing player racing animations
+/// </summary>
 public class PlayerAnimation : MonoBehaviour
 {
     Animator playerAnimator;
     SpriteRenderer playerSprite;
 
+    // The names of animations as specified in the PlayerAnimator asset.
     public readonly string IdleTurn = "IdleTurn";
     public readonly string IdleBack = "IdleBack";
     public readonly string AccelerateTurn = "AccelerateTurn";
@@ -19,14 +23,27 @@ public class PlayerAnimation : MonoBehaviour
 
     string currentAnimation = "";
     bool driftFlip;
+    /// <summary>
+    /// Gets necessary components for playing animations
+    /// </summary>
     private void Awake()
     {
         playerAnimator = GetComponentInChildren<Animator>();
         playerSprite = GetComponentInChildren<SpriteRenderer>();
     }
+    /// <summary>
+    /// Enables animations relating to movement, making them playable.
+    /// </summary>
     public void EnableMoveAnimations() { moveAnimations = true; }
+    /// <summary>
+    /// Disables animations relating to movement, making them unplayable.
+    /// </summary>
     public void DisableMoveAnimations() { moveAnimations = false; }
-
+    /// <summary>
+    /// Returns true if an animation has ended.
+    /// </summary>
+    /// <param name="animation">The name of an animation as a string. </param>
+    /// <returns></returns>
     public bool AnimationHasEnded(string animation)
     {
         if (!playerAnimator.GetCurrentAnimatorStateInfo(0).loop &&
@@ -34,13 +51,13 @@ public class PlayerAnimation : MonoBehaviour
             playerAnimator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1)
         {
             // Animation has ended, do something
-            //Debug.Log("Animation ended!");
             return true;
         }
         return false;
     }
+    // *** IDLE Animation Methods ***
     /// <summary>
-    /// Idle animation functions
+    /// Sets the animation to idle, depending on the direction the player is facing.
     /// </summary>
     public void SetIdle() 
     {
@@ -50,12 +67,18 @@ public class PlayerAnimation : MonoBehaviour
         else if (PlayerActionManager.Instance.moveValue.x < 0) { playerSprite.flipX = false; SetIdleTurn(); }
         else { SetIdleBack(); }
     }
+    /// <summary>
+    /// Handles default idle, full back view of player.
+    /// </summary>
     public void SetIdleBack() 
     { 
         if(currentAnimation == IdleBack) { return; }
         playerAnimator.Play(IdleBack);
         currentAnimation = IdleBack;
     }
+    /// <summary>
+    /// Handles 3/4 idle view of player.
+    /// </summary>
     public void SetIdleTurn() 
     {
         if (currentAnimation == IdleTurn) { return; }
@@ -63,8 +86,9 @@ public class PlayerAnimation : MonoBehaviour
         currentAnimation = IdleTurn;
     }
     
+    // *** ACCELERATE Animation Methods ***
     /// <summary>
-    /// Accelerate animation functions
+    /// Sets the animation to accelerate, depending on the direction the player is facing.
     /// </summary>
     public void SetAccelerate() 
     {
@@ -74,21 +98,27 @@ public class PlayerAnimation : MonoBehaviour
         else if (PlayerActionManager.Instance.moveValue.x < 0) { playerSprite.flipX = false; SetAccelerateTurn(); }
         else { SetAccelerateBack(); }
     }
+    /// <summary>
+    /// Handles the default accelerate, full back view of player.
+    /// </summary>
     public void SetAccelerateBack() 
     {
         if (currentAnimation == AccelerateBack) { return; }
         playerAnimator.Play(AccelerateBack);
         currentAnimation = AccelerateBack;
     }
+    /// <summary>
+    /// Handles 3/4 accelerate view of player.
+    /// </summary>
     public void SetAccelerateTurn() 
     {
         if (currentAnimation == AccelerateTurn) { return; }
         playerAnimator.Play(AccelerateTurn);
         currentAnimation = AccelerateTurn;
     }
-    
+    // *** DRIFT Animation Methods ***
     /// <summary>
-    /// Drift animation functions
+    /// Sets the animation for the start of drift, depending on the direction the player is drifting.
     /// </summary>
     public void SetStartDrift()
     {
@@ -99,6 +129,9 @@ public class PlayerAnimation : MonoBehaviour
         playerAnimator.Play(StartDrifting);
         currentAnimation = StartDrifting;
     }
+    /// <summary>
+    /// Plays an animation when drifting ends, depending on the direction the player faced when drifting started.
+    /// </summary>
     public void SetEndDrift()
     {
         if(currentAnimation == EndDrifting) { return; }
@@ -106,6 +139,9 @@ public class PlayerAnimation : MonoBehaviour
         playerAnimator.Play(EndDrifting);
         currentAnimation = EndDrifting;
     }
+    /// <summary>
+    /// Plays the animation that plays while continuing drifting.
+    /// </summary>
     public void SetDrifting()
     {
         if(currentAnimation == Drifting) { return; }
@@ -113,5 +149,9 @@ public class PlayerAnimation : MonoBehaviour
         playerAnimator.Play(Drifting);
         currentAnimation = Drifting;
     }
+    /// <summary>
+    /// Checks if the EndDrifting animation is still ongoing.
+    /// </summary>
+    /// <returns> True if the EndDrifting animation is still playing. False otherwise. </returns>
     public bool IsEndingDrift() { return (currentAnimation == EndDrifting && !AnimationHasEnded(EndDrifting)); }
 }
