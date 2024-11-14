1661c4dc7cb29b11748601b4434c00d182749ee9
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-13 21:18:32 -0800

    Update AudioManager with summary comments

--- Assets/Scripts/Audio/AudioManager.cs
+++ Assets/Scripts/Audio/AudioManager.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
 
+/// <summary>
+/// AudioManager contains methods for playing different sounds, music, sfx from two arrays of AudioSources.
+/// </summary>
 public class AudioManager : MonoBehaviour
 {
     private static AudioManager _instance;
@@ -12,6 +15,10 @@ public class AudioManager : MonoBehaviour
     [SerializeField] private AudioSource[] sfx;
     //[SerializeField] int levelMusicToPlay;
     public AudioMixerGroup musicMixer, sfxMixer;
+
+    /// <summary>
+    /// Singleton functionality, ensuring one instance of AudioManager, that is not destroyed between scenes.
+    /// </summary>
     private void Awake()
     {
         if(_instance != null && _instance != this)
@@ -30,16 +37,28 @@ public class AudioManager : MonoBehaviour
     //    PlayMusic(levelMusicToPlay);
     //}
     
+    /// <summary>
+    /// Plays music at index musicToPlay from the music property.
+    /// </summary>
+    /// <param name="musicToPlay"> An index for accessing elements of music property. </param>
     public void PlayMusic(int musicToPlay)
     {
         music[musicToPlay].Play();
     }
 
+    /// <summary>
+    /// Plays sfx at index sfxToPlay from the sfx property.
+    /// </summary>
+    /// <param name="sfxToPlay"> An index for accessing elements of sfx property. </param>
     public void PlaySFX(int sfxToPlay) 
     {
         sfx[sfxToPlay].Play();
     }
 
+    /// <summary>
+    /// Pauses sfx at index sfxToPlay from sfx property.
+    /// </summary>
+    /// <param name="sfxToPlay"> An index for accessing elements of sfx property. </param>
     public void PauseSFX(int sfxToPlay)
     {
         sfx[sfxToPlay].Pause();
