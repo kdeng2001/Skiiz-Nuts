c6ffc3e22b8f6cbb4b93df864e405ebe78bd257e
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-14 15:21:45 -0800

    add summary comments to FrameRate class

--- Assets/Scripts/Testing/FrameRate.cs
+++ Assets/Scripts/Testing/FrameRate.cs
@@ -2,15 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// FrameRate is a component that when attached to any gameObject, will make the frame rate equal to the frameRate property.
+/// It's useful for debugging issues that arise different frame rate between users.
+/// </summary>
 public class FrameRate : MonoBehaviour
 {
-    // Start is called before the first frame update
     [SerializeField] public int frameRate = 5;
+    /// <summary>
+    /// Sets the frame rate to what frameRate property is initially.
+    /// </summary>
     private void Start()
     {
         Application.targetFrameRate = frameRate;
     }
-
+    /// <summary>
+    /// Changes the frame rate if there are changes to frameRate value.
+    /// This is useful for changing frameRate during playmode.
+    /// </summary>
     private void Update()
     {
         if(Application.targetFrameRate != frameRate)
