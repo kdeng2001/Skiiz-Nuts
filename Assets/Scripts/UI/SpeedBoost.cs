a87b8bda5696dcb470db5b6ffc3df04d4d8227bb
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-16 14:04:01 -0800

    add summary comments to SpeedBoost

--- Assets/Scripts/UI/SpeedBoost.cs
+++ Assets/Scripts/UI/SpeedBoost.cs
@@ -2,21 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// SpeedBoost contains functionality for the duration and end of the speed boost animation (since it loops).
+/// </summary>
 public class SpeedBoost : MonoBehaviour
 {
     public float timeElapsed;
     public float durationTime;
 
+    /// <summary>
+    /// Hides the gameObject containing the speed boost animation.
+    /// </summary>
     private void Awake()
     {
         gameObject.SetActive(false);
     }
 
+    /// <summary>
+    /// Resets the timeElapsed value to 0 when the gameObject is enabled.
+    /// </summary>
     private void OnEnable()
     {
         timeElapsed = 0f;
     }
 
+    /// <summary>
+    /// Keeps track of time passed while animation is playing.
+    /// If timeElapsed is greater than the specified duration of the animation loop, the animation is finished.
+    /// </summary>
     private void Update()
     {
         if(timeElapsed > durationTime)
@@ -26,6 +39,14 @@ public class SpeedBoost : MonoBehaviour
         else if(isActiveAndEnabled){ timeElapsed += Time.deltaTime;}
     }
 
+    /// <summary>
+    /// Manually sets the timeElapsed value to time parameter.
+    /// </summary>
+    /// <param name="time"> The time. </param>
     public void SetTimeElapsed(float time) { timeElapsed = time; }
+    /// <summary>
+    /// Manually sets the durationTime to time parameter.
+    /// </summary>
+    /// <param name="time"> The time. </param>
     public void SetDurationTime(float time) { durationTime = time; }
 }
