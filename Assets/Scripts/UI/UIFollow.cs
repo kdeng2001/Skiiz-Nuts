ef0fc228e2239533e9d89342e2fd13bad96d36e3
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-16 14:27:15 -0800

    add summary comments to UIFollow

--- Assets/Scripts/UI/UIFollow.cs
+++ Assets/Scripts/UI/UIFollow.cs
@@ -2,14 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Ensures the UI element follows the position of the followTarget.
+/// </summary>
 public class UIFollow : MonoBehaviour
 {
     [SerializeField] private Transform followTarget;
+    /// <summary>
+    /// Follow the target at every frame.
+    /// </summary>
     private void Update()
     {
         UIFollowObject();
     }
 
+    /// <summary>
+    /// Sets the image screen position to the screen position of the followTarget.
+    /// </summary>
     private void UIFollowObject()
     {
         transform.position = RectTransformUtility.WorldToScreenPoint(
