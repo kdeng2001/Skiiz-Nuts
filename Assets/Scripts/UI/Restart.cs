4a62faf191d453096890bc164dbc6d485a2fec4e
kdeng2001 <85768217+kdeng2001@users.noreply.github.com>
2024-11-15 23:30:55 -0800

    add summary comments to Restart

--- Assets/Scripts/UI/Restart.cs
+++ Assets/Scripts/UI/Restart.cs
@@ -3,8 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
+/// <summary>
+/// Restart contains a method for reloading the current scene.
+/// </summary>
 public class Restart : MonoBehaviour
 {
+    /// <summary>
+    /// Reloads the current scene.
+    /// </summary>
     public void RestartScene()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
